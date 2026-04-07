8ab1cadbb010ea0fa0bc2c141884371d1c166007
fighty91 <106462950+fighty91@users.noreply.github.com>
2026-04-07 22:18:17 +0700

    update header

--- src/app/(DashboardLayout)/customer2/page.tsx
+++ src/app/(DashboardLayout)/customer2/page.tsx
@@ -4,6 +4,7 @@ import PageContainer from '@/app/(DashboardLayout)/components/container/PageCont
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { useEffect } from 'react';
 import { getContactsFromAPI } from '@/lib/features/contact/action';
+import { IconCirclePlus } from '@tabler/icons-react';
 
 const Customer = () => {
   const dispatch = useAppDispatch()
@@ -29,15 +30,40 @@ const Customer = () => {
 
   return (
     <PageContainer title="Customer Page" description="this is Customer page">
-      <Typography variant="h3" color="#616161" minWidth="170px">
-        Customer
-      </Typography>
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Grid size={{ sm: 6, md: 10 }}>
+          <Stack direction="row" spacing={0} alignItems="center">
+            <Typography variant="h3" color="#616161">
+              Customer
+            </Typography>
+          </Stack>
+        </Grid>
+        {/* BUTTON CREATE */}
+        <Grid >
+          <Stack direction="row" spacing={1} alignItems="center" justifyContent="flex-end">
+            {/* <Button variant="contained" sx={{ width: 100 }} component={Link} to="create"> */}
+            <Button>
+              add customer &nbsp;<IconCirclePlus width="20" height="20" />
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+      <br />
       <br />
 
       <Grid container spacing={3}>
         {contacts.map((contact, index) => (
           <Grid key={index} size={{ xs: 12, sm: 6, md: 4}}>
             <Card variant="outlined">
+              <CardActionArea>
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     {contact.name}
@@ -58,6 +84,7 @@ const Customer = () => {
                     {contact.phone}
                   </Typography>
                 </CardContent>
+              </CardActionArea>
             </Card>
           </Grid>
         ))}
