787aa324e445693d5a37394a16b9e228033eeb13
fighty91 <106462950+fighty91@users.noreply.github.com>
2026-04-07 12:45:05 +0700

    update action

--- src/lib/features/action.ts
+++ src/lib/features/action.ts
@@ -1,7 +1,11 @@
-// import { child, get, getDatabase, onValue, ref } from "firebase/database";
-import { onValue, ref } from "firebase/database";
+// import { child, DataSnapshot, get, getDatabase, onValue, ref } from "firebase/database";
+import { DataSnapshot, onValue, ref } from "firebase/database";
 import database from "@/config/database";
 import { setContact } from "./contact/contactSlice";
+import { Dispatch } from "@reduxjs/toolkit";
+// import { useAppDispatch } from "../hooks";
+// const dispatch = useAppDispatch();
+
 const serviceApp = 'accountingProfit';
 
 // export const getContactFromAPI = (id) => () => {
@@ -40,25 +44,38 @@ const serviceApp = 'accountingProfit';
 //   });
 // };
 
-
-export const getContactsFromAPI = () => {
-  return function(dispatch) {
-    return new Promise((resolve) => {
-      const starCountRef = ref(database, `${serviceApp}/contacts`);
-      onValue(starCountRef, (snapshot) => {
-        let temp = { ...snapshot.val() },
-          contacts = [];
-        for (let x in temp) {
-          temp[x].id = x;
-          contacts.push(temp[x]);
-        }
-        contacts.sort((a, b) => new Intl.Collator('de').compare(a.name, b.name));
-        // contacts.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
-        dispatch(setContact({ value: contacts }));
-        resolve(contacts);
-      });
-    });
-    
-  };
+// types.ts (atau letakkan di file yang sama)
+export interface Contact {
+  id: string;
+  name: string;
+  [key: string]: any; // Untuk properti tambahan lainnya
 }
 
+
+export const getContactsFromAPI = () => {
+  return (dispatch: Dispatch): Promise<Contact[]> => {
+    return new Promise((resolve) => {
+      const starCountRef = ref(database, `${serviceApp}/contacts`);
+      onValue(starCountRef, (snapshot: DataSnapshot) => {
+        const data = snapshot.val();
+
+        if(!data) {
+          dispatch(setContact({ value: [] }));
+          return resolve([]);
+        }
+
+        const contacts: Contact[] = Object.keys(data).map((key) => ({
+          ...data[key],
+          id: key,
+        }));
+
+        // sortir menggunakan Intl.Collator
+        const collator = new Intl.Collator('de');
+        contacts.sort((a, b) => collator.compare(a.name, b.name));
+
+        dispatch(setContact({ value: contacts }));
+        resolve(contacts);
+      })
+    })
+  }
+}
