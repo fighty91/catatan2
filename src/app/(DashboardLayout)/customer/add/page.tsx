8ba3836be107179e72cf0741075f2b32aa7e1816
fighty91 <106462950+fighty91@users.noreply.github.com>
2026-04-11 08:36:21 +0700

    update add customer

--- src/app/(DashboardLayout)/customer/add/page.tsx
+++ src/app/(DashboardLayout)/customer/add/page.tsx
@@ -27,10 +27,17 @@ export default function AddCustomer() {
   const [contact, setContact] = useState<any>({
     name: '',
     address: '',
-    npwp: ''
+    npwp: '',
+    phone: {
+      value: '',
+      country: {}
+    },
+    phone2: {
+      value: '',
+      country: {}
+    }
   });
   const [nameValidation, setNameValidation] = useState<any>(null);
-  const [phone, setPhone] = useState<any>(0);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
@@ -41,8 +48,12 @@ export default function AddCustomer() {
     console.log(e)
   }
 
-  const handleChangePhone = (e: any) => {
-    console.log(e)
+  const handleChangePhone = (value: string, country: object, e: any) => {
+    const { name } = e.target;
+    let data = { ...contact };
+    
+    data[name] = { value, country };
+    setContact(data);
   }
 
   useEffect(() => {
@@ -89,116 +100,275 @@ export default function AddCustomer() {
       </Grid>
       <br />
       <br />
-      <Grid container spacing={3} sx={{ height: "450px"}}>
-        <Grid size={{ xs: 12, md: 6 }} sx={{ height: "100%" }}>
-          <Card variant="outlined" sx={{ height: "100%" }}>
-            <CardHeader
-              title="Biodata"
-              sx={{ pr: 1, pb: 0, height: '64px' }}
-            />
-            <CardContent sx={{ pt: 1 }}>
-              <Grid container spacing={3}>
-                <Grid size={{ xs: 12, sm: 6 }}>
-                  <TextField
-                    autoFocus
-                    error={nameValidation ? true : false}
-                    helperText={nameValidation}
-                    name="name"
-                    label="Name"
-                    type="text"
-                    fullWidth
-                    variant="outlined"
-                    value={contact.name}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6 }}>
-                  <TextField
-                    name="npwp"
-                    label="NPWP"
-                    type="text"
-                    fullWidth
-                    variant="outlined"
-                    value={contact.npwp}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6 }}>
-                  <PhoneInput
-                    country="id"
-                    preferredCountries={["id"]}
-                    specialLabel="Phone"
-                    placeholder="Phone"
-                    value={phone}
-                    enableSearch
-                    onChange={handleChangePhone}
-                    countryCodeEditable={false}
-                    containerStyle={{
-                      width: '100%'
-                    }}
-                    inputStyle={{
-                      width: '100%',
-                      height: '53px', // atau '40px' untuk small
-                      // fontSize: '16px',
-                      borderColor: 'rgba(0, 0, 0, 0.23)',
-                      color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
-                      fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
-                      // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
-                    }}
-                    // buttonStyle={{
-                    //   borderRadius: '4px 0 0 4px',
-                    //   backgroundColor: 'transparent',
-                    //   borderRight: '1px solid rgba(0, 0, 0, 0.23)'
-                    // }}
-                  />
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6 }}>
-                  <PhoneInput
-                    country="id"
-                    preferredCountries={["id"]}
-                    specialLabel="Phone 2"
-                    placeholder="Phone 2"
-                    enableSearch
-                    // value={phone}
-                    // onChange={handleChangePhone}
-                    countryCodeEditable={false}
-                    containerStyle={{
-                      width: '100%'
-                    }}
-                    inputStyle={{
-                      width: '100%',
-                      height: '53px', // atau '40px' untuk small
-                      // fontSize: '16px',
-                      borderColor: 'rgba(0, 0, 0, 0.23)',
-                      color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
-                      fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
-                      // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
-                    }}
-                    // buttonStyle={{
-                    //   borderRadius: '4px 0 0 4px',
-                    //   backgroundColor: 'transparent',
-                    //   borderRight: '1px solid rgba(0, 0, 0, 0.23)'
-                    // }}
-                  />
-                </Grid>
-                <Grid size={{ xs: 12, sm: 6 }}>
-                  <TextField
-                    name="address"
-                    label="Address"
-                    type="text"
-                    fullWidth
-                    variant="outlined"
-                    value={contact.address}
-                    onChange={handleChange}
-                  />
-                </Grid>
-              </Grid>
-              <br />
-              <Divider />
-            </CardContent>
-          </Card>
+      <Grid container spacing={3}>
+        <Grid
+          container
+          spacing={2}
+          size={{ xs: 12, md: 6 }}
+          sx={{
+            height: "100%",
+            border: "1px solid #6b728021",
+            borderRadius: "8px",
+            px: 2,
+            py: 3
+          }}
+        >
+          <Typography variant="h5" gutterBottom>
+            Biodata
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                autoFocus
+                error={nameValidation ? true : false}
+                helperText={nameValidation}
+                name="name"
+                label="Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                name="npwp"
+                label="NPWP"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.npwp}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <PhoneInput
+                value={contact.phone.value}
+                inputProps={{
+                  name: 'phone',
+                  required: true,
+                }}
+                onChange={handleChangePhone}
+                country="id"
+                preferredCountries={["id"]}
+                enableSearch
+                // countryCodeEditable={false}
+                enableAreaCodes
+                specialLabel="Phone"
+                placeholder="Phone"
+                containerStyle={{
+                  width: '100%'
+                }}
+                inputStyle={{
+                  width: '100%',
+                  height: '53px', // atau '40px' untuk small
+                  fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
+                  // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
+                }}
+                buttonStyle={{
+                  position: 'absolute'
+                }}
+                dropdownStyle={{
+                  maxWidth: '270px',
+                  zIndex: 1000
+                }}
+                searchStyle={{
+                  width: '95%',     // Agar search bar di dalam tidak overflow
+                  margin: '10px auto'
+                }}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <PhoneInput
+                value={contact.phone2.value}
+                inputProps={{
+                  name: 'phone2',
+                  required: true,
+                }}
+                onChange={handleChangePhone}
+                country="id"
+                preferredCountries={["id"]}
+                enableSearch
+                // countryCodeEditable={false}
+                enableAreaCodes
+                specialLabel="Phone 2"
+                placeholder="Phone 2"
+                containerStyle={{
+                  width: '100%'
+                }}
+                inputStyle={{
+                  width: '100%',
+                  height: '53px', // atau '40px' untuk small
+                  fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
+                  // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
+                }}
+                buttonStyle={{
+                  position: 'absolute'
+                }}
+                dropdownStyle={{
+                  maxWidth: '270px',
+                  zIndex: 1000
+                }}
+                searchStyle={{
+                  width: '95%',     // Agar search bar di dalam tidak overflow
+                  margin: '10px auto'
+                }}
+              />
+            </Grid>
+            
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                name="address"
+                label="Address"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.address}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
         </Grid>
-
+        <Grid
+          container
+          spacing={2}
+          size={{ xs: 12, md: 6 }}
+          sx={{
+            height: "100%",
+            border: "1px solid #6b728021",
+            borderRadius: "8px",
+            px: 2,
+            py: 3
+          }}
+        >
+          <Typography variant="h5" gutterBottom>
+            Position
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                autoFocus
+                error={nameValidation ? true : false}
+                helperText={nameValidation}
+                name="name"
+                label="Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                name="npwp"
+                label="NPWP"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.npwp}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <PhoneInput
+                value={contact.phone.value}
+                inputProps={{
+                  name: 'phone',
+                  required: true,
+                }}
+                onChange={handleChangePhone}
+                country="id"
+                preferredCountries={["id"]}
+                enableSearch
+                // countryCodeEditable={false}
+                enableAreaCodes
+                specialLabel="Phone"
+                placeholder="Phone"
+                containerStyle={{
+                  width: '100%'
+                }}
+                inputStyle={{
+                  width: '100%',
+                  height: '53px', // atau '40px' untuk small
+                  fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
+                  // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
+                }}
+                buttonStyle={{
+                  position: 'absolute'
+                }}
+                dropdownStyle={{
+                  maxWidth: '270px',
+                  zIndex: 1000
+                }}
+                searchStyle={{
+                  width: '95%',     // Agar search bar di dalam tidak overflow
+                  margin: '10px auto'
+                }}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <PhoneInput
+                value={contact.phone2.value}
+                inputProps={{
+                  name: 'phone2',
+                  required: true,
+                }}
+                onChange={handleChangePhone}
+                country="id"
+                preferredCountries={["id"]}
+                enableSearch
+                // countryCodeEditable={false}
+                enableAreaCodes
+                specialLabel="Phone 2"
+                placeholder="Phone 2"
+                containerStyle={{
+                  width: '100%'
+                }}
+                inputStyle={{
+                  width: '100%',
+                  height: '53px', // atau '40px' untuk small
+                  fontSize: '15px',           // Sedikit mengecilkan font agar kesan "pudar" lebih terasa
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  color: 'rgba(0, 0, 0, 0.7)', // Membuat teks input lebih pudar
+                  // opacity: 0.9                // Alternatif lain untuk memudarkan seluruh elemen input
+                }}
+                buttonStyle={{
+                  position: 'absolute'
+                }}
+                dropdownStyle={{
+                  maxWidth: '270px',
+                  zIndex: 1000
+                }}
+                searchStyle={{
+                  width: '95%',     // Agar search bar di dalam tidak overflow
+                  margin: '10px auto'
+                }}
+              />
+            </Grid>
+            
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <TextField
+                name="address"
+                label="Address"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={contact.address}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3} sx={{ height: "450px"}}>
         <Grid size={{ xs: 12, md: 6 }}>
           <Card variant="outlined" sx={{ height: "100%" }}>
             <CardHeader
